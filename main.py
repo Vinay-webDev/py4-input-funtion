a944ec8d5d2bf433ccad7cbf87840b6f918e5e6a
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-06 23:44:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 #INPUT FUNCTION
-#name = input("what is your name: ")
-#age =input("what is your age: ")
-#age=age+1
-#input("what is your age: ")
-#input("what is your occupation: ")
-#print("Hello "+name)
+name = input("what is your name: ")
+age =input("what is your age: ")
+age=age+1
+input("what is your age: ")
+input("what is your occupation: ")
+print("Hello "+name)
 
-#name=input("what is your name: ")
-#age=int(input("what is your age: "))
-#age=age+1
-#print("Your age is: "+str(age)+" years old")
+name=input("what is your name: ")
+age=int(input("what is your age: "))
+age=age+1
+print("Your age is: "+str(age)+" years old")
 
 
-#name=input("what is your name?: ")
-#age=int(input("what is your age?: "))
-#height=float(input("what is your height?: "))
+name=input("what is your name?: ")
+age=int(input("what is your age?: "))
+height=float(input("what is your height?: "))
 
-#print("Hello "+name)
-#print("Your age is: "+str(age)+" years old")
-#print("your  height is: "+str(height)+" cm tall")
+print("Hello "+name)
+print("Your age is: "+str(age)+" years old")
+print("your  height is: "+str(height)+" cm tall")
